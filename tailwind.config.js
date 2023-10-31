07fa33ee19b558d0994d6c369131ae552ab69875
David Kohalmi <killerkpoke@gmail.com>
2023-10-31 14:50:01 +0100

    add theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+    colors:{
+      'dark-green': '#285430',
+      'green': '#5F8D4E',
+      'light-green': '#A4BE7B',
+      'beige':'#E5D9B6'
+    }},
   },
   plugins: [require("daisyui")],
 }
