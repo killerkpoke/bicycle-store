be7c2934b3d03ac602de5428c7fa12c34509dba1
David Kohalmi <killerkpoke@gmail.com>
2023-10-31 15:24:42 +0100

    add app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from 'src/components/home/home.component';
+import { BikesComponent } from 'src/components/bikes/bikes.component';
+import { BikeDetailComponent } from 'src/components/bike-detail/bike-detail.component';
+import { BikepartsComponent } from 'src/components/bikeparts/bikeparts.component';
+import { BikepartDetailComponent } from 'src/components/bikepart-detail/bikepart-detail.component';
+import { RentbikeComponent } from 'src/components/rentbike/rentbike.component';
+import { RentbikeDetailComponent } from 'src/components/rentbike-detail/rentbike-detail.component';
+import { CartComponent } from 'src/components/cart/cart.component';
+import { AboutComponent } from 'src/components/about/about.component';
+import { LoginComponent } from 'src/components/login/login.component';
+import { AdminComponent } from 'src/components/admin/admin.component';
 
 const routes: Routes = [
-  // {path: '', redirectTo:'home', pathMatch:'full'},
-  // {path: 'home', component:},
-
-  // {path: 'bikes', component:},
-  // {path: 'bike/:id', component:},
-
-  // {path: 'rentbike', component:},
-  // {path: 'rentbike/:bikename', component:},
-
-  // {path: 'bikeparts', component:},
-  // {path: 'bikepart/:partname', component:},
-
-  // {path: 'cart', component:},
-  // {path: 'about', component:},
-
-  // {path: 'login', component:},
-  // {path: 'admin', component:},
+  {path: '', redirectTo:'home', pathMatch:'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'bikes', component: BikesComponent},
+  {path: 'bike/:id', component: BikeDetailComponent},
+  {path: 'rentbike', component: RentbikeComponent},
+  {path: 'rentbike/:bikename', component: RentbikeDetailComponent},
+  {path: 'bikeparts', component: BikepartsComponent},
+  {path: 'bikepart/:partname', component: BikepartDetailComponent},
+  {path: 'cart', component: AboutComponent},
+  {path: 'about', component: CartComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'admin', component: AdminComponent},
 ];
 
 @NgModule({
