8ff858efe82ee52b9121d4bddc46d06453b3a1e5
David Kohalmi <killerkpoke@gmail.com>
2023-10-31 15:28:07 +0100

    add component to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,33 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponentComponent } from 'src/components/navbar-component/navbar-component.component';
 
+import { HomeComponent } from 'src/components/home/home.component';
+import { BikesComponent } from 'src/components/bikes/bikes.component';
+import { BikeDetailComponent } from 'src/components/bike-detail/bike-detail.component';
+import { BikepartsComponent } from 'src/components/bikeparts/bikeparts.component';
+import { BikepartDetailComponent } from 'src/components/bikepart-detail/bikepart-detail.component';
+import { RentbikeComponent } from 'src/components/rentbike/rentbike.component';
+import { RentbikeDetailComponent } from 'src/components/rentbike-detail/rentbike-detail.component';
+import { CartComponent } from 'src/components/cart/cart.component';
+import { AboutComponent } from 'src/components/about/about.component';
+import { LoginComponent } from 'src/components/login/login.component';
+import { AdminComponent } from 'src/components/admin/admin.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponentComponent
+    NavbarComponentComponent,
+    HomeComponent,
+    BikesComponent,
+    BikeDetailComponent,
+    BikepartsComponent,
+    BikepartDetailComponent,
+    RentbikeComponent,
+    RentbikeDetailComponent,
+    CartComponent,
+    AboutComponent,
+    LoginComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
