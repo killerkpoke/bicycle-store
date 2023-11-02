fabd4eecdcfc0a59d63858b3936458a00785962e
David Kohalmi <killerkpoke@gmail.com>
2023-11-02 12:03:54 +0100

    define my-card and add mock data

--- src/components/bikes/bikes.component.ts
+++ src/components/bikes/bikes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Bike } from 'src/types/Bike';
 
 @Component({
   selector: 'app-bikes',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bikes.component.css']
 })
 export class BikesComponent implements OnInit {
+
+  cards:Array<Bike> = [
+    {name: "dolog 1", description: "random dolog", tags:["random", "another random"]},
+    {name: "dolog 2", description: "random dolog 2", tags:["random"]},
+    {name: "dolog 3", description: "random dolog 3", tags:["random"]},
+    {name: "dolog 4", description: "random dolog 4", tags:["random"]},
+  ]
 
   constructor() { }
 
