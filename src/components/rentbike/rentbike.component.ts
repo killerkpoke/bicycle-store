d49b12b0aabc9e8350172490896fe2d059833bc7
David Kohalmi <killerkpoke@gmail.com>
2023-11-02 15:30:40 +0100

    fill with data the rentbike page

--- src/components/rentbike/rentbike.component.ts
+++ src/components/rentbike/rentbike.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-rentbike',
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RentbikeComponent implements OnInit {
 
-  constructor() { }
+  cards:Array<any> = [
+    {id: 1, name: "rented bike 1", description: "random dolog", tags:["new", "BMX"]},
+    {id: 2, name: "rented bike 2", description: "random dolog 2", tags:["new"]},
+    {id: 3, name: "rented bike 3", description: "random dolog 3", tags:["used"]},
+    {id: 4, name: "rented bike 4", description: "random dolog 4", tags:["used"]},
+  ]
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
