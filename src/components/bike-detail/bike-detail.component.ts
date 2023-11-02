3abc9e116e42c0d79a9c071a91427e1b76e54660
David Kohalmi <killerkpoke@gmail.com>
2023-11-02 16:10:46 +0100

    create bike-detail

--- src/components/bike-detail/bike-detail.component.ts
+++ src/components/bike-detail/bike-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Bike } from 'src/types/Bike';
 
 @Component({
   selector: 'app-bike-detail',
@@ -7,7 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BikeDetailComponent implements OnInit {
 
-  constructor() { }
+  bike:Bike = {id: 1, name: 'kecske', description:'random', tags:['new', 'old', 'used', 'valami random']};
+  images: Array<any> = [
+    {id:1, src:'https://images.dog.ceo/breeds/setter-gordon/n02101006_3026.jpg'},
+    {id:2, src:'https://images.dog.ceo/breeds/setter-gordon/n02101006_3026.jpg'},
+    {id:3, src:'https://images.dog.ceo/breeds/setter-gordon/n02101006_3026.jpg'},
+
+  ];
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
